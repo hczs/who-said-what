0d8887267600826f9eba67aa273f3cc3dc41896d
cheng <hczshd@gmail.com>
2025-05-14 15:19:01 +0800

    feat: add async asr wav function

--- app/service/asr_service.py
+++ app/service/asr_service.py
@@ -12,6 +12,8 @@
 """
 __author__ = 'powercheng'
 
+import asyncio
+
 import numpy as np
 import sherpa_onnx
 from loguru import logger
@@ -25,7 +27,10 @@ class AsrService:
         self.asr_type: AsrEnum = AsrEnum.from_string(settings.models.asr)
         self.recognizer = None
 
-    def load_model(self):
+    def load_model(self) -> None:
+        if self.recognizer is not None:
+            logger.warning("asr model has been loaded")
+            return
         # load asr model
         if self.asr_type == AsrEnum.FIRE_RED_ASR:
             self.recognizer = sherpa_onnx.OfflineRecognizer.from_fire_red_asr(
@@ -43,9 +48,9 @@ class AsrService:
             raise ValueError(f"asr_type must be in {AsrEnum.values()}, current is {self.asr_type.value}")
         logger.info(f"load asr model success, asr_type: {self.asr_type.value}")
 
-    def asr_wav(self, wav_data: np.ndarray) -> tuple[str, str]:
+    def asr_wav_sync(self, wav_data: np.ndarray) -> tuple[str, str]:
         """
-        wav 文件数据识别
+        wav 文件数据识别（同步）
 
         :param wav_data: wav数据段
         :return: 文本情绪（生气（ANGRY）、高兴（HAPPY）、伤心（SAD）和中性（NEUTRAL）），文本内容
@@ -55,6 +60,15 @@ class AsrService:
         self.recognizer.decode_stream(asr_stream)
         result = asr_stream.result
         return result.emotion, result.text
+
+    async def asr_wav(self, wav_data: np.ndarray) -> tuple[str, str]:
+        """
+        wav 文件数据识别
+
+        :param wav_data: wav数据段
+        :return: 文本情绪（生气（ANGRY）、高兴（HAPPY）、伤心（SAD）和中性（NEUTRAL）），文本内容
+        """
+        return await asyncio.to_thread(self.asr_wav_sync, wav_data)
 
 
 asr_service = AsrService()
