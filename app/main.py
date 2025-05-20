cfb7bd00867e40bb4f9d7b57ac6d359e52015042
cheng <hczs88881215@163.com>
2025-05-20 16:30:04 +0800

    fix: fix repeat import example router bug

--- app/main.py
+++ app/main.py
@@ -26,8 +26,6 @@ app = FastAPI(lifespan=lifespan)
 # 添加请求日志中间件
 app.middleware("http")(log_request_middleware)
 
-app.include_router(example.router)
-
 # 动态导入 app.api 目录下的所有模块
 package = importlib.import_module("app.api")
 for _, module_name, _ in pkgutil.iter_modules(package.__path__):
