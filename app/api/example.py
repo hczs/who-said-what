b0d8cef6e43099ff40de54a945ad51b63d9b4151
cheng <hczs88881215@163.com>
2025-05-20 16:43:13 +0800

    refactor: unify response structure using common format

--- app/api/example.py
+++ app/api/example.py
@@ -14,8 +14,12 @@ __author__ = 'powercheng'
 
 from fastapi import APIRouter
 
+from app.schemas.example import ExampleResponse
+from app.schemas.response import Response
+
 router = APIRouter(prefix="/example", tags=["example"])
 
-@router.get("/")
-async def test():
-    return {"message": "Hello World"}
+
+@router.get("/", response_model=Response[ExampleResponse])
+async def test_example():
+    return Response.success(data=ExampleResponse(id=1, message="hello"))
