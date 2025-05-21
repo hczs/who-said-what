304aeec31e53d0014a397f9ee22fe0b22901790d
cheng <hczs88881215@163.com>
2025-05-21 09:51:11 +0800

    feat: parallel load the model at the start of the app

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -12,19 +12,29 @@
 """
 __author__ = 'powercheng'
 
+import asyncio
 import time
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from loguru import logger
 
 from app.core.loguru_config import init_loguru
+from app.service.asr_service import asr_service
+from app.service.diarization_service import diarization_service
+from app.service.embedding_service import embedding_service
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # 日志初始化
     start_time = time.time()
-    init_loguru()
+    # 日志初始化
+    init_loguru(),
+    await asyncio.gather(
+        # 模型加载
+        asyncio.to_thread(asr_service.load_model),
+        asyncio.to_thread(diarization_service.load_model),
+        asyncio.to_thread(embedding_service.load_model),
+    )
     cost_time = time.time() - start_time
     logger.info(f'App init completed, cost {cost_time:.4}s')
     yield
